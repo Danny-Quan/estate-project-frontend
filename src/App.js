037185d249463b52ea967cb8a9e9d60eaf18ccf2
DANNY QUAN <danielsah118@gmail.com>
2023-02-24 20:49:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Footer from "./components/landingPage/Components/Footer";
 import Pages from "./components/pages/Pages";
-import Scroll from './Scroll'
-import {BrowserRouter} from 'react-router-dom'
+import Scroll from "./Scroll";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-    <Scroll/>
-    <div>
-    <Pages/>
-    <Footer/>
-    </div>
+      <Scroll />
+      <div>
+        <Pages />
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
