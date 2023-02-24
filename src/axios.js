8e87571ecb77cf1add79fc156d556b5325910644
DANNY QUAN <danielsah118@gmail.com>
2023-02-24 20:35:18 +0000

    first commit

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 // https://estate-project-backend.herokuapp.com
+// https://aparweb-backend.onrender.com/
 // http://localhost:8001
 const instance = axios.create({
-  baseURL: "http://localhost:8001",
+  baseURL: "https://aparweb-backend.onrender.com/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
